12deea5a8b6245429c662039a3eaa19926682a1b
DanielJochem <daniel.jochem@gmail.com>
2015-06-12 20:46:17 +1000

    Init commit

---
+++ Interactive-Game.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading;
+
+namespace InteractiveFictionGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		//Initialization
+			
+		}
+	}
+}
